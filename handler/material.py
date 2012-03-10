48289c9a9047970920cfae42726556fc46ba2bb9
amigoradio <amigo.shaw@gmail.com>
2012-03-10 14:40:53 +0800

    打印post的数据

--- handler/material.py
+++ handler/material.py
@@ -7,6 +7,11 @@ class MaterialHandler(BaseHandler):
         self.render('material.html')
 
     def post(self, *args, **kwargs):
+        print('name=%s'%self.get_argument('name'))
+        print('sumPrice=%s'%self.get_argument('sumPrice'))
+        print('sumNum=%s'%self.get_argument('sumNum'))
+        print('size=%s'%self.get_argument('size'))
+        print('desc=%s'%self.get_argument('description'))
         self.redirect('/materialList')
 
 class MaterialListHandler(BaseHandler):
