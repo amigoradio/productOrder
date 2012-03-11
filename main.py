9b9b1ca4e17b51bb6615a085fc2db3eee1b225d3
amigoradio <amigo.shaw@gmail.com>
2012-03-11 14:40:28 +0800

    将db传入handler

--- main.py
+++ main.py
@@ -9,16 +9,20 @@ import handler.material
 import handler.index
 from tornado.options import define,options
 from setting import *
+from pymongo import Connection
 
 define('port',default=8888,help='run server on 8888 port',type=int)
+
+conn = Connection('localhost')
+db = conn.shaw
 
 def main():
     tornado.options.parse_command_line()
     all_handler = [
-        (r'/',handler.index.IndexHandler),
-        (r'/product',handler.product.ProductHandler),
-        (r'/material',handler.material.MaterialHandler),
-        (r'/materialList',handler.material.MaterialListHandler),
+        (r'/',handler.index.IndexHandler,dict(db=db)),
+        (r'/product',handler.product.ProductHandler,dict(db=db)),
+        (r'/material',handler.material.MaterialHandler,dict(db=db)),
+        (r'/materialList',handler.material.MaterialListHandler,dict(db=db)),
     ]
     application = tornado.web.Application(all_handler, **setting)
     server = tornado.httpserver.HTTPServer(application)
