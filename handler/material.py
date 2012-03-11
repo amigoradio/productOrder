8b349ad4cdc726d82f304e991087b88357eced38
amigoradio <amigo.shaw@gmail.com>
2012-03-11 14:39:49 +0800

    引入mongodb
    发布原料功能

--- handler/material.py
+++ handler/material.py
@@ -7,13 +7,30 @@ class MaterialHandler(BaseHandler):
         self.render('material.html')
 
     def post(self, *args, **kwargs):
-        print('name=%s'%self.get_argument('name'))
-        print('sumPrice=%s'%self.get_argument('sumPrice'))
-        print('sumNum=%s'%self.get_argument('sumNum'))
-        print('size=%s'%self.get_argument('size'))
-        print('desc=%s'%self.get_argument('description'))
+        name = self.get_argument('name')
+        sumPrice = self.get_argument('sumPrice')
+        sumNum = self.get_argument('sumNum')
+        size = self.get_argument('size')
+        description = self.get_argument('description')
+        unitPrice = round((sumPrice)/int(sumNum),2)
+        print('name=%s'%name)
+        print('sumPrice=%s'%sumPrice)
+        print('sumNum=%s'%sumNum)
+        print('size=%s'%size)
+        print('desc=%s'%description)
+        posts = {'name':name,
+                 'sumPrice':sumPrice,
+                 'sumNum':sumNum,
+                 'size':size,
+                 'description':description,
+                 'unitPrice':unitPrice,
+                 }
+        self.db.material.save(posts)
         self.redirect('/materialList')
 
 class MaterialListHandler(BaseHandler):
     def get(self, *args, **kwargs):
-        self.render('materialList.html')
+        entries = self.db.material.find()
+#        for i in entries:
+#            print(i.get('name'))
+        self.render('materialList.html',entries=list(entries))
